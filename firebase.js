89325e3da93fc26140fcc2b3933668c4d765b21e
Darragh <daradona10@gmail.com>
2021-07-19 20:05:57 +0100

    checkout cart

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDV_Jy0uUhwiUtFlhlhrYq6wKl0dsJu9Ss",
+  authDomain: "amzn-2-yt-80fe2.firebaseapp.com",
+  projectId: "amzn-2-yt-80fe2",
+  storageBucket: "amzn-2-yt-80fe2.appspot.com",
+  messagingSenderId: "1088420519106",
+  appId: "1:1088420519106:web:70e1dbc6a73e1b2269ef5b",
+};
